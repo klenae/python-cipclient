83fb2269e08a73359373d442bf3124b5425cd31f
Katherine Lenae <klenae@gmail.com>
2020-03-15 01:51:28 +0000

    updated setup.py (closes #6)

--- setup.py
+++ setup.py
@@ -1,23 +1,43 @@
-import setuptools
+"""A setuptools based setup module.
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
 
-setuptools.setup(
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
     name="python-cipclient",
     version="0.0.1",
     author="Katherine Lenae",
     author_email="klenae@gmail.com",
-    description="cip client",
+    description="""A Python-based socket client for communicating
+                   with Crestron control processors via CIP.""",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license="MIT",
     url="https://github.com/klenae/python-cipclient",
-    packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Home Automation",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     py_modules=["cipclient"],
+    keywords="development cip home-automation",
 )
